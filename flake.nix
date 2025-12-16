667c094b52f31dbad7673e8aabbf61c55e646fbe
zstg <o0vckutt@duck.com>
2025-12-16 14:20:58 +0530

    Update flake

--- flake.nix
+++ flake.nix
@@ -6,27 +6,29 @@
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
+    stratpapers = pkgs.stdenv.mkDerivation {
+          pname = "stratos-wallpapers";
+          version = "1.0-2";
+          src = ./.;
+          installPhase = ''
+            mkdir -p $out/share/backgrounds/StratOS
+            cp -r usr/share/backgrounds/StratOS/* $out/share/backgrounds/StratOS/
+          '';
+          # Optionally, add meta attributes
+          meta = {
+            description = "StratOS wallpapers";
+            authors = with pkgs.lib.authors; [ zstg ];
+            license = pkgs.lib.licenses.gpl3;
+            platforms = pkgs.lib.platforms.all;
+          };
+        };
   in {
-    packages.${system}.stratos-wallpapers = pkgs.stdenv.mkDerivation {
-      pname = "stratos-wallpapers";
-      version = "1.0-2";
-      src = ./.;
-      installPhase = ''
-        mkdir -p $out/share/backgrounds/StratOS
-        cp -r usr/share/backgrounds/StratOS/* $out/share/backgrounds/StratOS/
-      '';
-      # Optionally, add meta attributes
-      meta = {
-        description = "StratOS wallpapers";
-        authors = with pkgs.lib.authors; [ zstg ];
-        license = pkgs.lib.licenses.gpl3;
-        platforms = pkgs.lib.platforms.all;
-      };
+    packages.${system} = {
+      stratos-wallpapers = stratpapers; 
+      # Expose the wallpapers path for other flakes
+      default = stratpapers;
     };
-
-    # Expose the wallpapers path for other flakes
-    packages.${system}.default = self.packages.${system}.stratos-wallpapers;
     # For other flakes to use as an input
-    wallpapers = self.packages.${system}.stratos-wallpapers;
+    wallpapers = stratpapers;
   };
 }
